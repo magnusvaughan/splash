dfaf2ab70ada2dad40b0db15d80336865bf4a3ef
mvaughan <magnus.vaughan@gmail.com>
2019-05-20 22:41:48 +0100

    Made first list and detail page views with a bit of styling

--- splash/urls.py
+++ splash/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import WordlistListView, WordlistDetailView
 
 urlpatterns = [
-    path('wordlist/<int:pk>/', WordlistDetailView.as_view(), name='post_detail'),
+    path('wordlist/<int:pk>/', WordlistDetailView.as_view(), name='wordlist_detail'),
     path('', WordlistListView.as_view(), name='home'),
 ]
