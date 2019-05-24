b6578108988078f425bf8506a2e67c2bf7993a3d
mvaughan <magnus.vaughan@gmail.com>
2019-05-24 21:25:47 +0100

    Added static root

--- splash_project/settings.py
+++ splash_project/settings.py
@@ -141,9 +141,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
 AUTH_USER_MODEL = 'users.CustomUser'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
