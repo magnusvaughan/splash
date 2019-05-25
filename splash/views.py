551cfaac7d091dd96c41f3fa8e82ed8eacbf901b
mvaughan <magnus.vaughan@gmail.com>
2019-05-25 21:29:58 +0100

    Added all phrase totals page

--- splash/views.py
+++ splash/views.py
@@ -26,8 +26,32 @@ class WordlistDetailView(DetailView):
 class PhraseListView(ListView):
     model = Phrase
     template_name = 'phrase.html'
-    context_object_name = 'phrases'
+    context_object_name = 'wordtotals'
     ordering = ['phrase']
+
+    def get_context_data(self, **kwargs):
+        context = super(PhraseListView, self).get_context_data(**kwargs)
+        phrases = Phrase.objects.all()
+
+        wordtotals_object = {}
+
+        for phrase in phrases:
+            wordtotals = phrase.wordtotal_set.all()
+            count = 0
+
+            for wordtotal in wordtotals:
+                count = count + wordtotal.count
+                wordtotals_object[phrase.phrase] = count
+
+        import operator
+
+        sorted_wordtotals = sorted(wordtotals_object.items(), key=operator.itemgetter(1), reverse=True)
+
+        context.update(
+            {'wordtotals': sorted_wordtotals
+        })
+                  
+        return context
 
 class PhraseDetailView(DetailView):
     model = Phrase
