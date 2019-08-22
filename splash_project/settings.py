4a672e62786e14b1bb850a8f36e0db672caa239b
mvaughan <magnus.vaughan@gmail.com>
2019-08-22 21:16:23 +0100

    Installed cachealot

--- splash_project/settings.py
+++ splash_project/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'splash.apps.SplashConfig',
-     'analytical',
+    'cachalot',
+    'analytical',
 ]
 
 MIDDLEWARE = [
