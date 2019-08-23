8021f24b7abef6ce044c3b11b1be4749d6138952
mvaughan <magnus.vaughan@gmail.com>
2019-08-23 18:57:28 +0100

    Disaabled memcached in config

--- splash_project/settings.py
+++ splash_project/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'splash.apps.SplashConfig',
-    'cachalot',
+    # 'cachalot',
     'analytical',
     'debug_toolbar',
 ]
@@ -161,49 +161,49 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 GOOGLE_ANALYTICS_PROPERTY_ID = 'UA-137553389-2'
 
-def get_cache():
-  import os
-  try:
-    servers = os.environ['MEMCACHIER_SERVERS']
-    username = os.environ['MEMCACHIER_USERNAME']
-    password = os.environ['MEMCACHIER_PASSWORD']
-    return {
-      'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-        # TIMEOUT is not the connection timeout! It's the default expiration
-        # timeout that should be applied to keys! Setting it to `None`
-        # disables expiration.
-        'TIMEOUT': None,
-        'LOCATION': servers,
-        'OPTIONS': {
-          'binary': True,
-          'username': username,
-          'password': password,
-          'behaviors': {
-            # Enable faster IO
-            'no_block': True,
-            'tcp_nodelay': True,
-            # Keep connection alive
-            'tcp_keepalive': True,
-            # Timeout settings
-            'connect_timeout': 2000, # ms
-            'send_timeout': 750 * 1000, # us
-            'receive_timeout': 750 * 1000, # us
-            '_poll_timeout': 2000, # ms
-            # Better failover
-            'ketama': True,
-            'remove_failed': 1,
-            'retry_timeout': 2,
-            'dead_timeout': 30,
-          }
-        }
-      }
-    }
-  except:
-    return {
-      'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-      }
-    }
+# def get_cache():
+#   import os
+#   try:
+#     servers = os.environ['MEMCACHIER_SERVERS']
+#     username = os.environ['MEMCACHIER_USERNAME']
+#     password = os.environ['MEMCACHIER_PASSWORD']
+#     return {
+#       'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+#         # TIMEOUT is not the connection timeout! It's the default expiration
+#         # timeout that should be applied to keys! Setting it to `None`
+#         # disables expiration.
+#         'TIMEOUT': None,
+#         'LOCATION': servers,
+#         'OPTIONS': {
+#           'binary': True,
+#           'username': username,
+#           'password': password,
+#           'behaviors': {
+#             # Enable faster IO
+#             'no_block': True,
+#             'tcp_nodelay': True,
+#             # Keep connection alive
+#             'tcp_keepalive': True,
+#             # Timeout settings
+#             'connect_timeout': 2000, # ms
+#             'send_timeout': 750 * 1000, # us
+#             'receive_timeout': 750 * 1000, # us
+#             '_poll_timeout': 2000, # ms
+#             # Better failover
+#             'ketama': True,
+#             'remove_failed': 1,
+#             'retry_timeout': 2,
+#             'dead_timeout': 30,
+#           }
+#         }
+#       }
+#     }
+#   except:
+#     return {
+#       'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+#       }
+#     }
 
-CACHES = get_cache()
+# CACHES = get_cache()
