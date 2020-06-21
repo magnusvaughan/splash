1bdd483f1983edd257df9258105c5c18a5532bde
mvaughan <magnus.vaughan@gmail.com>
2020-06-21 20:49:59 +0100

    Added tailwind (npx tailwindcss build  static/frontend/css/style.css -o static/frontend/compiled/main.css)

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -7,6 +7,24 @@ module.exports = {
           use: {
             loader: "babel-loader"
           }
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'style-loader',
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                importLoaders: 1,
+              }
+            },
+            {
+              loader: 'postcss-loader'
+            }
+          ]
         }
       ]
     }
