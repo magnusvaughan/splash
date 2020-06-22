1c06bf43b3182d26a6b15fdad89985c4207538dd
mvaughan <magnus.vaughan@gmail.com>
2020-06-22 08:34:13 +0100

    Full-width table

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -35,24 +35,46 @@ class App extends Component {
 
   render() {
     return (
-      <table className="table-auto">
-        <thead>
-          <tr>
-            <th className="px-4 py-2">Phrase</th>
-            <th className="px-4 py-2">Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.data.map((phraselist) => {
-            return (
-              <tr className="bg-gray-100" key={phraselist.id}>
-                <td class="border px-4 py-2">{phraselist.phrase}</td>
-                <td class="border px-4 py-2">{phraselist.count}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div class="flex flex-col">
+        <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+          <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+            <table class="min-w-full">
+              <thead>
+                <tr>
+                  <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Phrase
+                  </th>
+                  <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Count
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="bg-white">
+                {this.state.data.map((phraselist) => {
+                  return (
+                    <tr key={phraselist.id}>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div class="flex items-center">
+                          <div class="ml-4">
+                            <div class="text-sm leading-5 text-gray-900">
+                              {phraselist.phrase}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div class="text-sm leading-5 text-gray-900">
+                          {phraselist.count}
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     );
   }
 }
