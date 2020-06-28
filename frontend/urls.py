ce4df1bbb840ce6bfdc734f7cbd665f19e5e56bc
mvaughan <magnus.vaughan@gmail.com>
2020-06-28 17:24:57 +0100

    Fixed wildcard Django routing for react router.

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,10 +5,3 @@ from . import views
 urlpatterns = [
     path('', views.index ),
 ]
-
-urlpatterns += [
-    # match the root
-    path(r'^$', views.index),
-    # match all other pages
-    path(r'^(?:.*)/?$', views.index),
-]
--- splash_project/urls.py
+++ splash_project/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('splash.urls')),
-    path('', include('frontend.urls')),
+    re_path(r'^(?:.*)/?$', include('frontend.urls')),
 ]
